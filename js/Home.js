f001e505805fd4ac58f0426cee0ebe8ef31312e5
Rimantas <belauzas@yahoo.com>
2021-02-15 16:49:45 +0200

    Comments

---
+++ js/Home.js
@@ -0,0 +1,25 @@
+// IMPORTS
+/* header */
+/* hero */
+/* about me */
+/* services */
+/* resume */
+/* freelance */
+/* portfolio */
+/* testimonials */
+/* blogs */
+/* contact me */
+/* footer */
+
+// EXECUTION
+/* header */
+/* hero */
+/* about me */
+/* services */
+/* resume */
+/* freelance */
+/* portfolio */
+/* testimonials */
+/* blogs */
+/* contact me */
+/* footer */
